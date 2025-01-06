4c2edfade5b9b9e621046b8f6d61c90e317bdb93
Pradeep <pv4042609@gmail.com>
2025-01-06 15:43:32 +0530

    utils added

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from src.Red_Wine import logger
 
 logger.info("My name is Pradeep")
+logger.info("working on ml project")
+
+from box import ConfigBox
+
+data = ConfigBox({"name": "example"})
+print(data.name)  
