42500ec59f7c4650b26585c731a28f1005952901
Pradeep <pv4042609@gmail.com>
2025-01-08 10:32:03 +0530

    data ingestion pipeline created

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello pradeep")
+from Red_Wine import logger
+from Red_Wine.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
